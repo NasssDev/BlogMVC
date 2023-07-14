b5bfae6561868521b546a97aa704f278b0040932
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-07-14 21:34:44 +0200

    debug

--- public/index.php
+++ public/index.php
@@ -54,7 +54,7 @@ foreach ($routesObj as $route) {
 if (!$matched) {
     $filePath = __DIR__ . $_SERVER['REQUEST_URI'];
     var_dump($filePath);
-    die;
+    echo "<br/>$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$";
 
     if (is_file($filePath)) {
         $contentType = mime_content_type($filePath);
