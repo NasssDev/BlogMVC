56f2c994f98d7345d273893f9cce474f02f9af6b
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-07-15 18:53:02 +0200

    fi css

--- public/views/showOne.php
+++ public/views/showOne.php
@@ -1,5 +1,5 @@
 <!-- affiche un article en entier -->
-<div class="blog-slider-one">
+<div class="blog-slider">
     <div class="blog-slider__wrp swiper-wrapper">
         <?php
         if ($article->getStatut()) :
